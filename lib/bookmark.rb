51f95b51743a46e7439163fbcb6ab6caa9191b81
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-03-24 12:13:53 +0000

    Removed comment from viewing_bookmarks_spec.rb and changed bookmarks.rb so it doesn't add the string 'URL' to @bookmarks

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -6,7 +6,6 @@ class Bookmark
     db = (ENV['RACK_ENV'] == 'test' ? 'bookmark_manager_test' : 'bookmark_manager')
     conn = PG.connect( dbname: db )
     conn.exec( "SELECT * FROM bookmarks" ) do |result|
-      @bookmarks << "URL"
       result.each do |row|
          @bookmarks << "%s" %
           row.values_at('url')
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -5,7 +5,6 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'A user can see bookmarks' do
-    # allow(Bookmark).to receive(:all).and_return(Bookmark.all('bookmark_manager_test'))
     visit '/bookmarks'
     expect(page).to have_content('http://www.makersacademy.com')
     expect(page).to have_content('http://www.google.com')
